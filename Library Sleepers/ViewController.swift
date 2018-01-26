820594d7983e3d98dd3f5ed1261e5fed85f2427d
Vineet Joshi <vineet0713@gmail.com>
2018-01-26 15:40:18 -0800

    Replace if-let statements with guard statements for better readability.

--- Library Sleepers/ViewController.swift
+++ Library Sleepers/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var photoTitleLabel: UILabel!
     @IBOutlet weak var grabImageButton: UIButton!
     
+    var currentImageIndex: Int = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -56,38 +58,76 @@ class ViewController: UIViewController {
         let request = URLRequest(url: url)
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if error == nil {
-                // print(data!)
-                if let data = data {
-                    do {
-                        let parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
-                        
-                        if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:Any] {
-                            if let photoArray = photosDictionary ["photo"] as? [[String:Any]] {
-                                let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
-                                let photoDictionary = photoArray[randomPhotoIndex] as [String:Any]
-                                
-                                if let imageURLString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String,
-                                    let imageTitleString = photoDictionary[Constants.FlickrResponseKeys.Title] as? String {
-                                    
-                                    let imageURL = URL(string: imageURLString)
-                                    if let imageData = try? Data(contentsOf: imageURL!) {
-                                        performUIUpdatesOnMain {
-                                            self.photoImageView.image = UIImage(data: imageData)
-                                            self.photoTitleLabel.text = imageTitleString
-                                            self.setUIEnabled(enabled: true)
-                                        }
-                                    }
-                                    
-                                }
-                            }
-                        }
-                        
-                    } catch {
-                        print("Could not parse the data as JSON: \(data)")
-                        return
-                    }
-                }
+            if error != nil {
+                print("There was an error with your request.")
+                return
+            }
+            
+            // check for HTTP response
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, (statusCode >= 200 && statusCode <= 299) else {
+                print("Your request returned a status code other than 2xx.")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data was returned.")
+                return
+            }
+            
+            var parsedResult: [String:Any]!
+            
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+            } catch {
+                print("Could not parse the data as JSON: \(data)")
+                return
+            }
+            
+            // check if Flickr returned an error
+            guard let flickrStatus = parsedResult[Constants.FlickrResponseKeys.Status] as? String, flickrStatus == Constants.FlickrResponseValues.OKStatus else {
+                print("Flickr API returned an error.")
+                return
+            }
+            
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:Any] else {
+                print("Could not find key \(Constants.FlickrResponseKeys.Photos).")
+                return
+            }
+            
+            guard let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String:Any]] else {
+                print("Could not find key \(Constants.FlickrResponseKeys.Photo).")
+                return
+            }
+            
+            // makes sure that the newly displayed image will be different than current one:
+            var randomPhotoIndex = self.currentImageIndex
+            while randomPhotoIndex == self.currentImageIndex {
+                randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+            }
+            self.currentImageIndex = randomPhotoIndex
+            
+            let photoDictionary = photoArray[randomPhotoIndex] as [String:Any]
+            
+            guard let imageURLString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String else {
+                print("Could not find key \(Constants.FlickrResponseKeys.MediumURL).")
+                return
+            }
+            
+            guard let imageTitleString = photoDictionary[Constants.FlickrResponseKeys.Title] as? String else {
+                print("Could not find key \(Constants.FlickrResponseKeys.Title).")
+                return
+            }
+            
+            let imageURL = URL(string: imageURLString)
+            guard let imageData = try? Data(contentsOf: imageURL!) else {
+                print("Could not get image data.")
+                return
+            }
+            
+            performUIUpdatesOnMain {
+                self.photoImageView.image = UIImage(data: imageData)
+                self.photoTitleLabel.text = imageTitleString
+                self.setUIEnabled(enabled: true)
             }
         }
         
